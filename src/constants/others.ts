1b02ede70d412042509e1cdc763ea6d2e1e57d42
Horas Marolop Amsal Siregar <66661134+marloxxx@users.noreply.github.com>
2026-03-06 15:41:09 +0700

    Update others.ts

--- src/constants/others.ts
+++ src/constants/others.ts
@@ -6,4 +6,4 @@ export const HERO_PAGE_QUOTE = 'Crafting solutions through the art of code'
 
 export const CURRENT_STATUS = 'Currently looking for freelance opportunities.'
 
-export const RESUME_LINK = 'https://drive.google.com/file/d/1puTgSesnCj1qWgNOG_uIftckcCGq4dDJ/view?usp=sharing'
+export const RESUME_LINK = 'https://drive.google.com/file/d/1-CH96hvE5R8LXQwOgvBUspeCcw4adsIC/view?usp=sharing'
